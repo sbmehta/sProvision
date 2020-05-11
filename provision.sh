a00c4dfe5fd582fe8fcb0c367b1ec69cca31300a
samarmehta <sbmehta@gmail.com>
2020-05-11 14:39:36 -0400

    first draft provision scripts

---
+++ provision.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# provision.sh
+
+case $1 in
+    dotfiles)
+	echo "\n"
+	echo "Linking dotfiles ..."
+	ln -s sProvision/.zshrc      $HOME
+	ln -s sProvision/.emacs      $HOME
+	ln -s sProvision/.profile    $HOME
+	ln -s sProvision/.bashrc     $HOME
+	ln -s sProvision/.gitconfig  $HOME
+	echo "\n"
+	;;
+    ubuntu0)
+	echo "\n"
+	echo "Installing minimal software ..."
+	apt update && apt upgrade
+	apt install zsh                    # preferred environment
+	apt install curl wget              # data transfer
+	apt install lsb-release            # misc utilities
+
+	chsh -s '/bin/zsh' "${USER}"
+	echo "\n"
+	;;
+    ubuntu1)
+	apt install -y gcc make            # dev toolchain
+	apt install -y emacs               # preferred environment
+	
+	apt install -y software-properties-common    # tools for adding/removing PPAs
+	;;
+    help)
+	echo "\n"
+	echo "provision.sh dotfiles        Link dotfiles in home directory."
+	echo "provision.sh ubuntu0         (sudo) Set up Ubuntu v>16 environment for the first time; minimal space/bandwidth requirements."
+	echo "provision.sh ubuntu1         (sudo) Run after ubuntu0 to add larger packages; ~500MB."
+	echo "provision.sh help            Help string; this command."
+	echo "\n"
+	;;
+esac
+
+
---
+++ provision_ubuntu0.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# bootstrap_ubuntu0.sh
+
+echo "\n"
+echo "Getting provisioning script ..."
+
+apt update
+apt upgrade
+
+apt install -y git
+
+
