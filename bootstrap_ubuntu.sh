a80abdaf6dc51f62e9211113bff58e51d2732dc1
samarmehta <sbmehta@gmail.com>
2020-05-12 23:05:13 -0400

    fixed repo creation in bootstrap(?)

--- bootstrap_ubuntu.sh
+++ bootstrap_ubuntu.sh
@@ -11,16 +11,20 @@ if [[ $(id -u) -eq 0 ]] ; then
 fi
 
 if [[ ! -f ~/.ssh/id_rsa_github ]] ; then
-   printf "ERROR: Requires private key ~/.ssh/id_rsa_github\n\n"
-   exit 1
+    printf "ERROR: Requires my private key:"
+    printf "       scp samar@samarmehta.com:/home/samar/.ssh/id_rsa_github ~/.ssh"
+    exit 1
 else
-   ssh-add -l | grep L5Q7R8TIRv1/cszJwSPlrLbsGzhCu+dKF2QUH2Aq2D8 || ssh-add ~/.ssh/id_rsa_github
+    eval $(ssh-agent)
+    ssh-add ~/.ssh/id_rsa_github
+   #ssh-add -l | grep L5Q7R8TIRv1/cszJwSPlrLbsGzhCu+dKF2QUH2Aq2D8 || ssh-add ~/.ssh/id_rsa_github
 fi
 
 printf "Upgrading packages ..."
-#sudo apt update
-#sudo apt upgrade
+sudo apt update
+sudo apt upgrade
 sudo apt install -y git
+
 
 if [[ ! -d ~/sProvision ]] ; then
     mkdir ~/sProvision
@@ -28,10 +32,8 @@ fi
 cd ~/sProvision
 if [[ ! $(git rev-parse --is-inside-work-tree) ]] ; then
     printf "Installing provisioning script ...\n"
-    git init
-    git remote add origin git@github.com:sbmehta/sProvision.git
-    git branch --set-upstream-to=origin/master master
-    git pull
+    cd ..
+    git clone git@github.com:sbmehta/sProvision.git
 else
     git remote update
     if [[ $(git status --porcelain --untracked-files=no) ]] ; then
