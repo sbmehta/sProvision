6defa6d3da395e495236dfc0d1a7b4ed905db261
samarmehta <sbmehta@gmail.com>
2020-06-14 20:34:02 -0400

    big refactor, everything now run as user, added keyscan for samarmehta.com, github.com

--- provision_ubuntu.sh
+++ provision_ubuntu.sh
@@ -1,63 +1,61 @@
 #!/usr/bin/env bash
 
-# provision_ubuntu.sh
+## provision_ubuntu.sh
 
-set -e
+#set -e
 
 echo
 
-assert_user() {
-    case $1 in
-	root)
-	    if [ "$UID" -ne 0 ] && [ "$EUID" -ne 0 ] ; then
-		echo "ERROR: must be run with root privileges."
-		exit 1
-	    fi	    
-	    ;;
-	user)
-	    if [ "$UID" -eq 0 ] || [ "$EUID" -eq 0 ] ; then
-		echo "ERROR: can not be run with root privileges."
-		exit 1
-	    fi
-	    ;;
-	sudo)
-	    if [ "$EUID" -ne 0 ] || [ -z "$SUDO_USER" ] ; then  # EUID is 0 even if called as sudo?
-		echo "ERROR: must be called using sudo from a user account."
-		exit 1
-	    fi
-	    ;;
-    esac
+make_symlinks () {
+    echo "Linking dotfiles (backup if exists) ..." 
+
+    # should these be source'd instead of symlinked to allow local mods?
+    ln -sfb $HOME/sProvision/.bashrc     $HOME
+    ln -sfb $HOME/sProvision/.emacs      $HOME
+    ln -sfb $HOME/sProvision/.gitconfig  $HOME
+    ln -sfb $HOME/sProvision/.profile    $HOME
+    ln -sfb $HOME/sProvision/.zshrc      $HOME
+    ln -sfb $HOME/sProvision/.p10k.zsh   $HOME
+    
+    ln -sfb $HOME/sProvision/sshconfig   $HOME/.ssh/config
 }
 
 
+if [ "UID" -eq 0 ] ; then
+    echo
+    echo "WARNING: running as root can have unexpected effects."
+    echo	 
+fi
+
+
 case $1 in
-    test)
-	## Dummy case
-	assert_user "sudo"
-	;;
-    
-    bootstrap)   # call from desired uer account; interactive asks for passwords
+    bootstrap)   # interactive; asks for passwords
 	## (1) install git, (2) local copy my git key, (3) create local provision repo
 	## As a side effect, updates then cleans the apt cache
-	assert_user "user"       ## ?? what if want to use machine/container as root??
 	GITKEY="id_rsa_github"
 	PROVISION="sProvision"
-
+	KEYSERVER="samarmehta.com"
+	KEYUSER="samar"
+	
 	echo
 	echo "Enter LOCAL sudo password to make sure git up to date ..."
 	sudo apt update
-	sudo apt install -y git
+	sudo apt install -y git openssh-client
 	sudo apt autoclean
 
 	echo
-	echo "Enter SAMAR@SAMARMEHTA.COM password to fetch samar's github key ..."
-	rsync samar@samarmehta.com:/home/samar/.ssh/id_rsa_github\{,.pub\} $HOME/.ssh
+	echo "Enter $KEYUSER@$KEYSERVER password to fetch samar's github key ..."
+	ssh-keygen -R $KEYSERVER
+	ssh-keyscan -H $KEYSERVER >> ~/.ssh/known_hosts
+	rsync $KEYUSER@$KEYSERVER:/home/samar/.ssh/id_rsa_github\{,.pub\} $HOME/.ssh
 
 	echo
  	echo "Enter samar's ID_RSA_GITHUB password to test github key ..."
 	eval $(ssh-agent)
 	ssh-add $HOME/.ssh/$GITKEY
-
+	ssh-keygen -R github.com
+	ssh-keyscan -H github.com >> ~/.ssh/known_hosts
+	
 	echo
 	echo "Installing provisioning script ..."
 	if [[ -d $HOME/$PROVISION ]]; then     # if already there, offer to delete
@@ -72,67 +70,34 @@ case $1 in
 	    git clone git@github.com:sbmehta/$PROVISION.git $HOME/$PROVISION
 	    echo "Provision repo available at ~/$PROVISION"
 	fi
-
-	;;
-    
-    linkconfig)  # should these be source'd instead of symlinked to allow local mods?
-	assert_user "user"
-	echo "Linking dotfiles (backup if exists) ..." 
-
-	ln -sfb $HOME/sProvision/.bashrc     $HOME
-	ln -sfb $HOME/sProvision/.emacs      $HOME
-	ln -sfb $HOME/sProvision/.gitconfig  $HOME
-	ln -sfb $HOME/sProvision/.profile    $HOME
-	ln -sfb $HOME/sProvision/.zshrc      $HOME
-	ln -sfb $HOME/sProvision/.p10k.zsh   $HOME
-
-        ln -sfb $HOME/sProvision/sshconfig   $HOME/.ssh/config
-	;;
-    
-    setup_first)
-	assert_user "root"
-	apt update && apt upgrade -y
-	apt install -y zsh                           # preferred environment
-	apt install -y curl wget                     # data transfer
-	apt install -y neofetch keychain             # misc utilities
-	apt autoclean && apt autoremove
 	
-	cat $HOME/sProvision/wsl.conf >> /etc/wsl.conf  # simple defaults; no effect if not on wsl
+	;;
+    
+    setup_basic)
+	sudo apt update && apt upgrade -y
+	sudo apt install -y zsh                           # preferred environment
+	sudo apt install -y curl wget                     # data transfer
+	sudo apt install -y neofetch keychain             # misc utilities
+	sudo apt autoclean && apt autoremove
+
+	make_symlinks()
+	
+	sudo cat $HOME/sProvision/wsl.conf >> /etc/wsl.conf  # simple defaults; no effect if not on wsl
  
-   	chsh -s '/bin/zsh' "${USER}"
+   	sudo chsh -s '/bin/zsh' "${USER}"
 
 	echo "Note: Changes to wsl.conf require rebooting the distro."
 	;;
-    
-    setup_second)
-	assert_user "root"
 
-	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/powerlevel10k
-	
-	apt update && apt upgrade -y
-	apt install -y gcc make                      # dev toolchain
-	apt install -y emacs
-	apt install -y certbot ca-certificates       # certificates
-	apt install -y software-properties-common    # tools for adding/removing PPAs
-	apt install -y apt-transport-https
-
-	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-	add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-	apt update
-	apt -y install docker-ce
-	
-	add-apt-repository -y ppa:biosyntax/ppa     # prettification
-	apt update
-	apt install -y biosyntax-less
-
-	mkdir $HOME/.config/docker-zsh-completion
-	curl -L https://raw.githubusercontent.com/docker/docker-ce/master/components/cli/contrib/completion/zsh/_docker > $HOME/.config/docker-zsh-completion/_docker
+    relink_dotfiles)
+	make_simlinks()
 	;;
     
-    conda)
-	assert_user "user"
+    setup_dev)
 
-	# need to compare size if run pre-/post- other provision (i.e,. does this download a 2nd Python distribution?)
+	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/powerlevel10k
+
+	sudo apt update && apt upgrade -y
 	
 	# Start with Miniconda
 	cd $HOME
@@ -145,6 +110,34 @@ case $1 in
 	fi
 	bash miniconda.sh -b -p $HOME/miniconda
 	rm miniconda.sh
+	
+	## note: .zshrc & .bashrc should already have conda initialization; otherwise run "conda init 'shellname'"
+	
+	sudo apt install -y gcc make                      # dev toolchain
+	sudo apt install -y emacs                         # preferred editor
+	sudo apt install -y certbot ca-certificates       # certificates
+	sudo apt install -y software-properties-common    # tools for adding/removing PPAs
+	sudo apt install -y apt-transport-https
+
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+	sudo apt update
+	sudo apt -y install docker-ce
+	
+	sudo add-apt-repository -y ppa:biosyntax/ppa
+	sudo apt update
+	sudo apt install -y biosyntax-less
+
+	mkdir -p $HOME/.config
+	
+	mkdir $HOME/.config/docker-zsh-completion
+	curl -L https://raw.githubusercontent.com/docker/docker-ce/master/components/cli/contrib/completion/zsh/_docker > $HOME/.config/docker-zsh-completion/_docker
+
+	git clone https://github.com/esc/conda-zsh-completion $HOME/.config/conda-zsh-completion
+
+	;;
+	
+    setup_full)
 
 	conda config --add channels defaults
 	conda config --add channels bioconda
@@ -153,20 +146,15 @@ case $1 in
 	conda update -y --all
 	conda clean -y --all
 
-	mkdir -p $HOME/.config
-	cd $HOME/.config
-	git clone https://github.com/esc/conda-zsh-completion
-
-	## note: .zshrc & .bashrc should already have conda initialization; otherwise run "conda init 'shellname'"
 	;;
     
     help|*)
-	echo "provision_ubuntu.sh bootstrap      (user/interactive) Sets up git and clones my provisioning repo."
-	echo "provision_ubuntu.sh linkconfig     (user) Link home dir dotfiles & .ssh config to provisioning repo."
-	echo "provision_ubuntu.sh setup_first    (sudo) Installs {zsh,curl,wget,keychain,neofetch}; ~5-25 MB download, ~15-100MB disk. Sets default shell to zsh. links /etc/wsl.conf to provisioning repo."
-	echo "provision_ubuntu.sh setup_second   (sudo) Run after setup_first to add larger packages {emacs, docker, powerlevel10k, gcc, etc.}; ~280MB download, ~1GB disk."
-	echo "provision_ubuntu.sh conda          (user) Sets up a basic miniconda distribution; ?? size."
-	echo "provision_ubuntu.sh help           Help string; this command."
+	echo "provision_ubuntu.sh help              Help string; this command."
+	echo "provision_ubuntu.sh bootstrap         (interactive) Sets up git and clones my provisioning repo."
+	echo "provision_ubuntu.sh relink_dotfiles   Fixes links to ink home dir dotfiles & .ssh config to provisioning repo."
+	echo "provision_ubuntu.sh setup_basic       Installs {curl,keychain,neofetch,wget,zsh}; ~5-25 MB download, ~15-100MB disk. Sets default shell to zsh. links /etc/wsl.conf to provisioning repo."
+	echo "provision_ubuntu.sh setup_dev         Installs packages {conda, emacs, docker, gcc, powerlevel10k, gcc, etc.}; ~350MB download, ~1.5GB disk."
+	echo "provision_ubuntu.sh setup_full        Sets up python packages under conda"
 	;;
 esac
 
